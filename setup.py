aa8284dcaa6d9844b18ec90cbcdafc25516717ad
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-07-27 15:37:50 +0530

    requirements setup and logger file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AerialDetection',
+    version= '0.0.0',
+    author= 'Anushka Srivastava',
+    author_email= 'anushka.asthana1983@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
